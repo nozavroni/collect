712911adc97cfc0e7d098feb001aeff8b2437baf
Luke Visinoni <luke.visinoni@gmail.com>
2018-03-19 17:52:07 -0700

    Added Noz\to_array() function

--- src/functions.php
+++ src/functions.php
@@ -1,9 +1,48 @@
 <?php
 namespace Noz;
 
+use RuntimeException;
 use Traversable;
 
+/**
+ * Is a value traversable?
+ *
+ * @param mixed $value
+ *
+ * @return bool
+ */
 function is_traversable($value)
 {
     return is_array($value) || ($value instanceof Traversable);
+}
+
+/**
+ * Convert $items to an array
+ *
+ * @param mixed $items
+ *
+ * @throws RuntimeException if cannot be converted to an array
+ *
+ * @return array
+ */
+function to_array($items, $force = false)
+{
+    if (method_exists($items, 'toArray')) {
+        return $items->toArray();
+    } elseif (is_array($items)) {
+        return $items;
+    } elseif ($items instanceof Traversable) {
+        return iterator_to_array($items);
+    } elseif (is_string($items)) {
+        $json = json_decode($items, true);
+        if (is_array($json) && json_last_error() == JSON_ERROR_NONE) {
+            return $json;
+        }
+    }
+
+    if ($force) {
+        return (array) $items;
+    }
+
+    throw new RuntimeException(__FUNCTION__ . " could not convert items to an array");
 }
--- tests/Tests/FunctionsTest.php
+++ tests/Tests/FunctionsTest.php
@@ -2,7 +2,10 @@
 namespace Noz\Tests;
 
 use ArrayIterator;
-use function Noz\is_traversable;
+use Noz\Collection\Collection;
+use function Noz\is_traversable,
+             Noz\to_array;
+use SebastianBergmann\GlobalState\RuntimeException;
 use stdClass;
 
 /**
@@ -20,5 +23,72 @@ class FunctionsTest extends TestCase
         $this->assertFalse(is_traversable(new stdClass));
         $this->assertTrue(is_traversable($arr));
         $this->assertTrue(is_traversable($iter));
+    }
+
+    public function testToArrayConvertsCollectionToArray()
+    {
+        $arr = $this->getFixture('assoc');
+        $col = new Collection($arr);
+
+        $this->assertSame([
+            '1st' => 'first',
+            '2nd' => 'second',
+            '3rd' => 'third'
+        ], to_array($col));
+    }
+
+    public function testToArrayConvertsArrayToArray()
+    {
+        $arr = $this->getFixture('assoc');
+
+        $this->assertSame([
+            '1st' => 'first',
+            '2nd' => 'second',
+            '3rd' => 'third'
+        ], to_array($arr));
+    }
+
+    public function testToArrayConvertsTraversableToArray()
+    {
+        $arr = $this->getFixture('assoc');
+        $iter = new ArrayIterator($arr);
+
+        $this->assertSame([
+            '1st' => 'first',
+            '2nd' => 'second',
+            '3rd' => 'third'
+        ], to_array($arr));
+    }
+
+    public function testToArrayConvertsJsonStringArrayToArray()
+    {
+        $arr = $this->getFixture('assoc');
+        $json = json_encode($arr);
+
+        $this->assertInternalType('string', $json);
+        $this->assertJson($json);
+        $this->assertSame([
+            '1st' => 'first',
+            '2nd' => 'second',
+            '3rd' => 'third'
+        ], to_array($json));
+    }
+
+    public function testToArrayConvertsAnythingToArrayIfForceParamSetToTrue()
+    {
+        $this->assertSame([], to_array(null, true));
+        $this->assertSame([1,], to_array(1, true));
+        $this->assertSame([true,], to_array(true, true));
+        $this->assertSame([false,], to_array(false, true));
+        $this->assertSame(['',], to_array('', true));
+        $this->assertSame(['foo',], to_array('foo', true));
+    }
+
+    /**
+     * @expectedException RuntimeException
+     */
+    public function testToArrayThrowsExceptionIfCannotConvert()
+    {
+        to_array(null);
     }
 }
